c5b37d5de07c166a2e53f6ec8258060acde21d3f
carlosmrg4@gmail.com <carlosmrg4@gmail.com>
2023-09-04 23:43:55 -0500

    first commit

---
+++ Ecuacionespython1.py
@@ -0,0 +1,8 @@
+#Ejercicio 1
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+c = float(input("Ingrese el valor de c: "))
+
+r = ((c + 5)*(b**2 - 3*a*c)*a**2)/(4 * a)
+
+print("El resultado de la ecuacion es: ", r)
