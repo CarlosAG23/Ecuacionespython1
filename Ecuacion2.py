19c54af99e545c164e345cd5c8ac5bcbe8ce3012
carlosmrg4@gmail.com <carlosmrg4@gmail.com>
2023-09-05 23:17:42 -0500

    add new example

--- Ecuacion2.py
+++ Ecuacion2.py
@@ -3,5 +3,5 @@ a = float(input("Ingrese el valor de a: "))
 b = float(input("Ingrese el valor de b: "))
 
 a, b = b, a
-print("El valor de a es: ", a)
-print("El valor de b es: ", b)
+print("El valor a es: ", a)
+print("El valor b es: ", b)
