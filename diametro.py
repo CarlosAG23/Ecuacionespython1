865357693496499548fe3f828c22ef5895ce00a5
carlosmrg4@gmail.com <carlosmrg4@gmail.com>
2023-10-07 23:41:35 -0500

    add new examfle of python

---
+++ diametro.py
@@ -0,0 +1,6 @@
+diametro_mercurio = 1.516
+diametro_tierra = 3.959
+
+diferencia_diamentros = diametro_tierra - diametro_mercurio
+
+print("El diámetro de la Tierra es", diferencia_diamentros, "millas mayor que el de Mercurio.")
